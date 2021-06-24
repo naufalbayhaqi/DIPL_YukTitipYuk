57d54e834f2222ef6470cc5d6265ab5ece89e92c
naufalbayhaqi <36545035+naufalbayhaqi@users.noreply.github.com>
2021-06-24 19:35:45 +0700

    Delete koneksi.php

--- koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php 
-<!-- mengembalikan nilai fungsi berupa variable koneksi Ke mySQL -->
-$koneksi = mysqli_connect("localhost","root","","yuktitipyuk");
- 
-// Check connection
-if (mysqli_connect_errno()){
-	echo "Koneksi database gagal : " . mysqli_connect_error();
-}
-?>
